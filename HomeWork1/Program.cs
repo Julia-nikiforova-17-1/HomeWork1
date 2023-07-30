898f95cbc64d094b74d021d9c0fcb069c81fb4a4
Julia <nub06@rambler.ru>
2023-07-30 13:31:35 +0300

    Внесли изменения

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 1.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+ Console.WriteLine("Введите число");
+ int number1=Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Ведите число");
+ int number2=Convert.ToInt32(Console.ReadLine());
+ 
+ if (number1>number2)
+ {
+    Console.WriteLine(number1);
+ }
+
+ else 
+ {
+ Console.WriteLine(number2);
+
+ }
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//  Console.WriteLine("Введите число");
+ // int number1=Convert.ToInt32(Console.ReadLine());
+
+  // Console.WriteLine("Ведите число");
+  // int number2=Convert.ToInt32(Console.ReadLine());
+
+  // Console.WriteLine("Ведите число");
+  // int number3=Convert.ToInt32(Console.ReadLine());
+
+  // int max= number1;
+
+  // if (number2>max)
+  // {
+    // max=number2;
+    
+     // }
+
+    // if (number3>max)
+  // {
+    // max=number3;
+    
+     // }
+
+// Console.WriteLine(max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//  Console.WriteLine("Введите число");
+//  int number=Convert.ToInt32(Console.ReadLine());
+//  if (number%2==0)
+//  {
+//    Console.WriteLine("Yes");
+//  }
+//  else 
+//  {
+//   Console.WriteLine("No");
+//  }
+
+ //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//  Console.WriteLine("Введите число");
+//  int number=Convert.ToInt32(Console.ReadLine());
+//  while (number>1)
+//  {
+//   Console.WriteLine(number);
+//   number=number+1;
+//  }
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+// while (i <= number) 
+// {
+//     Console.WriteLine(i);
+//     i = i + 2;
+// }
